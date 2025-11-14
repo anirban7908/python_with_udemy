c26eadbf7d27d8edeab4b260f35f79dbbf2c2c3c
Anirban Choudhury <anirban.choudhury.7908@gmail.com>
2025-11-14 23:25:51 +0530

    day1 completed

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,3 @@
-# Write your code below this line 👇
-print("Hello \n world!")
+home_city = input("Enter Your city: ")
+pet_name = input("Enter Your pet name: ")
+print("Your band name could be: "+ home_city+ " " + pet_name)
