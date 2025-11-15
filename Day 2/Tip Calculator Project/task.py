78ef7ba0fa8360500b56740856ebf10c00ab6a79
Anirban Choudhury <anirban.choudhury.7908@gmail.com>
2025-11-15 23:05:09 +0530

    day two completed

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -2,5 +2,10 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
+tip_percent = tip/100
+bill_with_tip = bill+(bill*tip_percent)
+total = bill_with_tip/people
+round_num = round(total, 2)
+print(f"Your total bill with {tip}% of tip is: {round_num}")
 
 
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,8 @@
-len(12345)
+len('12345')
+
+print(type('abc'))
+print(type(123))
+print(type(True))
+print(type(123.87))
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
