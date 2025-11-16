5135da45b2f313d50ccc56ad2d54584bb2cdc18a
Anirban Choudhury <anirban.choudhury.7908@gmail.com>
2025-11-17 00:26:43 +0530

    3rd day completed

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,5 @@
+user_input = int(input("Enter Number: "))
+if user_input % 2 == 0:
+    print(f"The number {user_input} is Even")
+else:
+    print(f"The number {user_input} is odd")
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,5 +1,34 @@
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
+
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+bill = 0
+if size  == "S":
+    bill = 15
+
+elif size == "M":
+    bill = 20
+elif size == "L":
+    bill = 25
+else:
+    exit()
+
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+if pepperoni == 'Y':
+    if size == "S":
+        bill+=2
+    else:
+        bill += 3
+
+
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+if extra_cheese == "Y":
+    bill+=1
+
+print(f"Your final bill is: ${bill}.")
