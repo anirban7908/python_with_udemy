3ebb8141ef2f0b9fba5dbda0e2298984adb6ad81
Anirban Choudhury <anirban.choudhury.7908@gmail.com>
2025-11-30 21:59:19 +0530

    Day 7 completed

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -24,11 +24,13 @@ correct_letters = []
 while not game_over:
 
     # TODO-6: - Update the code below to tell the user how many lives they have left.
-    print("****************************<???>/6 LIVES LEFT****************************")
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
 
     # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
+    if guess in correct_letters:
+        print('You have already chosen this letter')
+        
     display = ""
 
     for letter in chosen_word:
@@ -46,13 +48,14 @@ while not game_over:
     #  e.g. You guessed d, that's not in the word. You lose a life.
 
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
 
         if lives == 0:
             game_over = True
 
             # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
-            print(f"***********************YOU LOSE**********************")
+            print(f"***********************YOU LOSE. The correct word is {chosen_word}.**********************")
 
     if "_" not in display:
         game_over = True
