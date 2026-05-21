7e90f51532d2da4ef56ca1e4255b541d73574d7c
Anirban Choudhury <anirban.choudhury.7908@gmail.com>
2026-05-22 00:21:26 +0530

    State game completed

--- U.S._States_Game/day-25-us-states-game-start/main.py
+++ U.S._States_Game/day-25-us-states-game-start/main.py
@@ -22,4 +22,28 @@ turtle.shape(image)
 
 # screen.exitonclick()
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+
+
+all_state = data.state.to_list()
+guessed_state = []
+
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="What's another state's name?").title()
+    
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in all_state:
+            if state not in guessed_state:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv") 
+        break
+    guessed_state.append(answer_state)
+    if answer_state in all_state:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(state_data.state.item())
