8a3038cfea391bb04d584a0b50aada0d6a0eec39
Anirban Choudhury <anirban.choudhury.7908@gmail.com>
2026-07-11 01:29:58 +0530

    day - 46 completed

--- Musical_time_machine/main.py
+++ Musical_time_machine/main.py
@@ -1,39 +1,78 @@
+import os
+import requests
+from bs4 import BeautifulSoup
 from ytmusicapi import YTMusic
+from pprint import pprint
 
-ytmusic = YTMusic()
-print(ytmusic)
-# from bs4 import BeautifulSoup
-# import requests
+# Optional Troubleshooting Step - Check for browser.json before doing anything else
+if not os.path.exists("browser.json"):
+    print("browser.json not found.")
+    print("You need to authenticate with YouTube Music first.")
+    print("Run one of these commands in your terminal from this project folder:\n")
+    print("  Mac:     pbpaste | ytmusicapi browser")
+    print("  Windows: ytmusicapi browser\n")
+    print("Copy the request headers from Firefox first.")
+    print("This will create browser.json.")
+    exit()
 
-# user_input = input("which year do you wamt to travel to? Provide date in this format YYYY-MM-DD: ")
-# header = {
-#     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-#     "Accept": "text/plain",
-#     "Content-Type": "text/plain"
-# }
-# user_input = "2026-04-18"
-# url = f"https://appbrewery.github.io/bakeboard-hot-100/{user_input}"
-# response = requests.get(url, headers=header)
-# response.raise_for_status()
-# web_data = response.text
+def get_songs(): 
+    # Scraping Bakeboard Hot 100
+    date = input(
+        "Which year do you want to travel to? Type the date in this format YYYY-MM-DD: "
+    )
+    url = f"https://appbrewery.github.io/bakeboard-hot-100/{date}"
+    response = requests.get(url)
 
-# soup = BeautifulSoup(web_data, "html.parser")
-# all_songs = soup.find_all('div',class_="chart-entry__info")
-# song_list=[]
-# songs_dict = {}
+    soup = BeautifulSoup(response.text, "html.parser")
+    song_names = [tag.getText().strip() for tag in soup.select("h3.chart-entry__title")]
+    return song_names[:10],date
+# print(song_names)
 
-# for song in all_songs:
-#     title = song.find('h3', class_='chart-entry__title').text
-#     artist = song.find('span', class_='chart-entry__artist').text
+
+
+def create_playlist(date):
+
+    # Verify authentication works
+    # pprint(playlists)
+    # print(f"Found {len(playlists)} playlists in your library.")
+
+    PLAYLIST_NAME = f"{date} Billboard 100"
+
+    playlists = yt.get_library_playlists()
+
+    playlist_id = None
+
+    for p in playlists:
+        if p["title"] == PLAYLIST_NAME:
+            playlist_id = p["playlistId"]
+            break
+
+    if playlist_id:
+        print(f"This playlist already exists: {playlist_id}.")
+    else:
+        playlist_id = yt.create_playlist(
+            PLAYLIST_NAME,
+            f"Playlist with the hottest songs from {date}",
+            privacy_status="PRIVATE",
+        )
+        print("Playlist created.")
     
-#     if title and artist:
-#         song_title = title.strip()
-#         song_artist = artist.strip()
-    
-#         songs_dict = {
-#             "title":song_title,
-#             "artist":song_artist,
-#         }
-        
-#         song_list.append(songs_dict)
-# print(song_list)
+    return playlist_id
+
+def add_songs(song_names,playlist_id):
+    for song in song_names:
+        try:
+            search_results = yt.search(song,filter='songs',limit=1)
+            yt.add_playlist_items(playlist_id, [search_results[0]["videoId"]])
+            print(f"Added: {song}")
+        except Exception as e:
+            print(f"skipped: {song} | reason: {e}")
+
+yt = YTMusic("browser.json")
+
+song_list, date = get_songs()
+if song_list:
+    playlist = create_playlist(date)
+    add_songs(song_list, playlist)
+else:
+    print(f"No Songs found on date {date}")
